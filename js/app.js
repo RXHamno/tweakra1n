2a872692f638011c8c2c725be24425af8ea88068
Balogh Gabriel Claudiu(Outland3r2007) <baloggabriel76@outlook.com>
2023-03-27 18:27:26 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,81 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+// Dom7
+var $ = Dom7;
+
+// Theme
+var theme = 'auto';
+if (document.location.search.indexOf('theme=') >= 0) {
+    theme = document.location.search.split('theme=')[1].split('&')[0];
+}
+
+// Init App
+var app = new Framework7({
+    id: 'com.Outlander.tweakra1n',
+    root: '#app',
+    theme: theme,
+    data: function() {
+        return {
+            user: {
+                firstName: 'John',
+                lastName: 'Appleseed',
+            },
+        };
+    },
+    methods: {
+        helloWorld: function() {
+            app.dialog.alert('Hello World!');
+        },
+    },
+    routes: routes,
+    popup: {
+        closeOnEscape: true,
+         swipeToClose: true,
+         closeByBackdropClick: true,
+    },
+    sheet: {
+        closeOnEscape: true,
+    },
+    popover: {
+        closeOnEscape: true,
+    },
+    actions: {
+        closeOnEscape: true,
+    },
+    // Register service worker
+  serviceWorker: {
+    path: 'service-worker.js',
+  },
+});
+}
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
